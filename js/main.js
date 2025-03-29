ed1860011f03c9bbfac74f031acb6a95f1f3ceb8
LivSterling <sterlingchapmanmade@gmail.com>
2025-03-29 02:30:44 -0400

    more

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
-
+  const url = `https://data.nasa.gov/resource/gvk9-iz74.json`
+  const weatherUrl = `https://api.weatherbit.io/v2.0/current?key=24dea913fd5046a8a94720d1ed563839&units=I`
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
